852ca79082865f5b4d224a79c1673936cbe33d8d
Ben Wills <benjaminwills047@gmail.com>
2026-09-10 09:38:52 +0100

    Break em

--- test_main.py
+++ test_main.py
@@ -2,4 +2,4 @@ from main import find_longest
 
 
 def test_find_longest():
-    assert find_longest(["yellow", "red", "blue", "green"]) == "red"
+    assert find_longest(["yellow", "red", "blue", "green"]) == "yellow"
